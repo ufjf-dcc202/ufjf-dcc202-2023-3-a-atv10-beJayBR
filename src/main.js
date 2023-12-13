68c46ef71072b0757b7c82e9be86e1d46442b141
Breno Junqueira <breno.junqueira@estudante.ufjf.br>
2023-12-13 16:46:55 -0300

    importando e usando os gets

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { setMaria, setJoao } from "./joaoEMaria";
+import { setMaria, setJoao, getMaria, getJoao } from "./joaoEMaria";
 
 console.log("MAIN");
 
 setJoao(-4);
 setMaria(8);
+
+let maria = getMaria();
+let joao = getJoao();
+console.log(maria,joao);
