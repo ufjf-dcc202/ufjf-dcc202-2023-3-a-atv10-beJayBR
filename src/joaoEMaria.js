ee796d90f7dc4ca1b3358999a7f13da7ed665b05
Breno Junqueira <breno.junqueira@estudante.ufjf.br>
2023-12-13 16:38:15 -0300

    criando os sets de joao e maria

--- src/joaoEMaria.js
+++ src/joaoEMaria.js
@@ -9,3 +9,18 @@ function getJoao(){
     return joao;
 }
 
+function setMaria(valor){
+    if(valor > 0){
+        maria = valor;
+    } else {
+        maria = 0;
+    }
+}
+
+function setJoao(valor){
+    if(valor > 0){
+        joao = valor;
+    } else {
+        joao = 0;
+    }
+}
